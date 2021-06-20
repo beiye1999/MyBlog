bc038c70687ae08405cfac23450587c8c253953a
Angus Lin <1719545163@qq.com>
2021-06-20 14:31:52 +0800

    初始化

---
+++ src/components/f-header.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<el-menu :default-active="activeIndex" class="d-flex" mode="horizontal" router="true" type="flex">
+			<el-menu-item class="mr-auto">贝爷</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-search"></i>&nbsp;搜索</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-home"></i>&nbsp;主页</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-FolderOpen"></i>&nbsp;时间轴</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-tags-o"></i>&nbsp;标签</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-archive"></i>&nbsp;分类</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-Link"></i>&nbsp;友链</el-menu-item>
+			<el-menu-item ><i class="el-blog-icon-heart"></i>&nbsp;关于</el-menu-item>
+		</el-menu>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+el-menu el-menu-item{
+	font-size: 18px;
+}
+.mr-auto{
+	padding-left: 50px;
+}
+</style>
