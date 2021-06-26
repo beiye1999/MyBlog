7d10362e258c0e3ff9856b48ccfcc768bdc05e9f
Angus Lin <1719545163@qq.com>
2021-06-26 20:28:30 +0800

    首页右侧css调整完毕

---
+++ src/components/personal.vue
@@ -0,0 +1,116 @@
+<template>
+	<div class="latest_articles">
+		<el-card class="box-card">
+			<div class="card-widget card-info">
+				<div class="card-info-avatar is-center"><img class="avatar-img"
+						src="https://ypcdnsave.cunzher.cn/img/logo.png!/format/webp"
+						onerror="this.onerror=null;this.src='/img/friend_404.gif'" alt="avatar">
+					<div class="author-info_name">cunzher</div>
+					<div class="author-info_description">Viewing life with medical ethics</div>
+				</div>
+				<div class="card-info-data">
+					<div class="card-info-data-item card-info-data-item-one is-center"><a href="/archives/">
+							<div class="headline">文章</div>
+							<div class="length-num">74</div>
+						</a></div>
+					<div class="card-info-data-item  card-info-data-item-two is-center"><a href="/tags/">
+							<div class="headline">标签</div>
+							<div class="length-num">69</div>
+						</a></div>
+					<div class="card-info-data-item is-center"><a href="/categories/">
+							<div class="headline">分类</div>
+							<div class="length-num">16</div>
+						</a></div>
+				</div>
+				<div class="call_me">
+					<a class="button--animated" id="card-info-btn" target="_blank" rel="noopener"
+						href="https://github.com/cunzher"><i class="el-blog-icon-github"
+							style="display: inline-block;padding-right: 8px;"></i><span>Follow Me</span></a>
+				</div>
+				<div class="card-info-social-icons is-center"><a class="social-icon" href="https://github.com/cunzher"
+						target="_blank" title="Github"><i class="el-blog-icon-github"></i></a><a class="social-icon"
+						href="mailto:cunzher@163.com" target="_blank" title="Email"><i
+							class="el-blog-icon-envelope"></i></a>
+				</div>
+			</div>
+		</el-card>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'friend'
+	}
+</script>
+
+<style scoped>
+	.avatar-img {
+		display: inline-block;
+		border-radius: 100px;
+		width: 50%;
+	}
+
+	.card-info-avatar {
+		text-align: center;
+	}
+
+	.author-info_name {
+		font-weight: bold;
+		padding: 10px 0px;
+		font-size: 20px;
+	}
+
+	.author-info_description {
+		font-size: 14px;
+	}
+
+	.card-info-data-item-one {
+		float: left;
+		width: 50px;
+		text-align: center;
+		margin-left: 32px;
+	}
+
+	.card-info-data-item-two {
+		float: left;
+		width: 110px;
+		text-align: center;
+	}
+
+	.card-info-data {
+		margin-top: 22px;
+	}
+
+	.length-num {
+		margin-top: 6px;
+	}
+
+	.call_me {
+		text-align: center;
+		margin: 22px 0px 0px 31px;
+		background-color: #49b1f5;
+		height: 32px;
+		line-height: 32px;
+		width: 82%;
+	}
+	
+	.box-card .call_me a {
+		color: white;
+	}
+
+	.box-card a {
+		text-decoration: none;
+		color: #4c4948;
+		font-size: 14px;
+	}
+
+	.card-info-social-icons {
+		text-align: center;
+		height: 30px;
+		line-height: 50px;
+	}
+	
+	.card-info-social-icons a{
+		margin: 0px 10px;
+	}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,7 +4,9 @@
 			<el-col :span="16">
 				<el-row class="art-item">
 					<el-card shadow="hover">
-						<h5><router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link></h5>
+						<h5>
+							<router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link>
+						</h5>
 						<el-row class="art-info d-flex align-items-center justify-content-start">
 							<div class="art-time"><i class="el-icon-time"></i>：2019-03-24</div>
 							<div class="d-flex align-items-center"><img class="tag" src="../assets/tag.png" />：
@@ -12,10 +14,12 @@
 							</div>
 						</el-row>
 						<el-row class="art-body">
-							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg"></div>
+							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg">
+							</div>
 							<div class="side-abstract">
 								<div class="art-abstract">
-									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照 在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
+									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照
+									在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
 								</div>
 								<div class="art-more">
 									<router-link to="/article" tag="span">
@@ -30,7 +34,9 @@
 				</el-row>
 				<el-row class="art-item">
 					<el-card shadow="hover">
-						<h5><router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link></h5>
+						<h5>
+							<router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link>
+						</h5>
 						<el-row class="art-info d-flex align-items-center justify-content-start">
 							<div class="art-time"><i class="el-icon-time"></i>：2019-03-24</div>
 							<div class="d-flex align-items-center"><img class="tag" src="../assets/tag.png" />：
@@ -38,10 +44,12 @@
 							</div>
 						</el-row>
 						<el-row class="art-body">
-							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg"></div>
+							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg">
+							</div>
 							<div class="side-abstract">
 								<div class="art-abstract">
-									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照 在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
+									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照
+									在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
 								</div>
 								<div class="art-more">
 									<router-link to="/article" tag="span">
@@ -56,7 +64,9 @@
 				</el-row>
 				<el-row class="art-item">
 					<el-card shadow="hover">
-						<h5><router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link></h5>
+						<h5>
+							<router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link>
+						</h5>
 						<el-row class="art-info d-flex align-items-center justify-content-start">
 							<div class="art-time"><i class="el-icon-time"></i>：2019-03-24</div>
 							<div class="d-flex align-items-center"><img class="tag" src="../assets/tag.png" />：
@@ -64,10 +74,12 @@
 							</div>
 						</el-row>
 						<el-row class="art-body">
-							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg"></div>
+							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg">
+							</div>
 							<div class="side-abstract">
 								<div class="art-abstract">
-									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照 在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
+									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照
+									在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
 								</div>
 								<div class="art-more">
 									<router-link to="/article" tag="span">
@@ -82,7 +94,9 @@
 				</el-row>
 				<el-row class="art-item">
 					<el-card shadow="hover">
-						<h5><router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link></h5>
+						<h5>
+							<router-link to="/article" tag="span" class="art-title">鼠标悬浮时显示</router-link>
+						</h5>
 						<el-row class="art-info d-flex align-items-center justify-content-start">
 							<div class="art-time"><i class="el-icon-time"></i>：2019-03-24</div>
 							<div class="d-flex align-items-center"><img class="tag" src="../assets/tag.png" />：
@@ -90,10 +104,12 @@
 							</div>
 						</el-row>
 						<el-row class="art-body">
-							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg"></div>
+							<div class="side-img hidden-sm-and-down"><img class="art-banner" src="../assets/vue.jpg">
+							</div>
 							<div class="side-abstract">
 								<div class="art-abstract">
-									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照 在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
+									Iconfont-国内功能很强大且图标内容很丰富的矢量图标库, 提供矢量图标下载、在快照
+									在小程序中使用阿里文字图标在小程序中使用阿里文字图标库前在小程序中使用阿里文字图标库前库前端开发的便捷工具 - AndrewNeo - CSDN博客
 								</div>
 								<div class="art-more">
 									<router-link to="/article" tag="span">
@@ -112,22 +128,25 @@
 				</div>
 			</el-col>
 			<el-col :span="6" class="hidden-sm-and-down" id="side" style="width: 24%;">
-				<div class="item">
+				<div class="item shadow">
+					<personal></personal>
+				</div>
+				<div class="item shadow">
 					<friend></friend>
 				</div>
-				<div class="item">
+				<div class="item shadow">
 					<latest_articles></latest_articles>
 				</div>
-				<div class="item">
+				<div class="item shadow">
 					<classification></classification>
 				</div>
-				<div class="item">
+				<div class="item shadow">
 					<tag></tag>
 				</div>
-				<div class="item">
+				<div class="item shadow">
 					<file></file>
 				</div>
-				<div class="item">
+				<div class="item shadow">
 					<website_information></website_information>
 				</div>
 			</el-col>
@@ -137,20 +156,22 @@
 </template>
 
 <script>
-	import tag from '../components/tag'//标签
-	import file from '../components/file'//归档
+	import tag from '../components/tag' //标签
+	import file from '../components/file' //归档
 	import friend from '../components/friend'
+	import personal from '../components/personal'
 	import classification from '../components/classification'
-	import latest_articles from '../components/latest_articles'//最新文章
-	import website_information from '../components/website_information'//网站资讯
+	import latest_articles from '../components/latest_articles' //最新文章
+	import website_information from '../components/website_information' //网站资讯
 	export default {
 		name: 'home',
 		components: {
-			friend,
 			tag,
-			latest_articles,
-			classification,
 			file,
+			friend,
+			personal,
+			classification,
+			latest_articles,
 			website_information
 		}
 	}
@@ -160,12 +181,12 @@
 	#side .item {
 		margin-bottom: 30px;
 	}
-	
+
 	.art-item {
 		margin-bottom: 30px;
 		position: relative;
 	}
-	
+
 	.art-item .star {
 		width: 60px;
 		height: 60px;
@@ -173,74 +194,87 @@
 		top: 0;
 		right: 0;
 	}
-	
+
 	img.tag {
 		width: 16px;
 		height: 16px;
 	}
-	
+
 	.art-title {
 		border-left: 3px solid #F56C6C;
 		padding-left: 5px;
 		cursor: pointer;
 	}
-	
+
 	.art-title:hover {
 		padding-left: 10px;
 		color: #409EFF;
 	}
-	
+
 	.art-time {
 		margin-right: 20px;
 	}
-	
+
 	.art-body {
 		display: flex;
 		padding: 10px 0;
 	}
-	
+
 	.side-img {
 		height: 150px;
 		width: 270px;
 		overflow: hidden;
 		margin-right: 10px;
 	}
-	
+
 	img.art-banner {
 		width: 100%;
 		height: 100%;
 		transition: all 0.6s;
 	}
-	
+
 	img.art-banner:hover {
 		transform: scale(1.4);
 	}
-	
+
 	.side-abstract {
 		flex: 1;
 		display: flex;
 		flex-direction: column;
 	}
-	
+
 	.art-abstract {
 		flex: 1;
 		color: #aaa;
 	}
-	
+
 	.art-more {
 		height: 40px;
 		display: flex;
 		justify-content: space-between;
 		align-items: flex-end;
 	}
-	
+
 	.art-more .view {
 		color: #aaa;
 	}
-	h5{
+
+	h5 {
 		font-size: 18px;
 	}
+
 	.pagination {
 		background-color: #F9F9F9;
+	}
+
+	.shadow {
+		background: #EEFF99;
+		transition-duration: 0.5s; 
+	}
+
+	.shadow:hover {
+		-webkit-box-shadow: #ccc 0px 20px 20px;
+		-moz-box-shadow: #ccc 0px 20px 20px;
+		box-shadow: #ccc 0px 20px 20px;
 	}
 </style>
